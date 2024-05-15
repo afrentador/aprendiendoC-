91cf12fe1c36c7328641ba983a1391898b915b79
Victor Mendoza <vmendozaespina@gmail.com>
2024-05-15 15:59:05 -0500

    Pirmeras lineas de C++

---
+++ inicios_en_c++/main.cpp
@@ -0,0 +1,9 @@
+//Mi primer hola mundo
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hola Mundo";
+    std::cin.get();
+    return 0;
+}
---
+++ tiposDeDatos_c++/tiposDeDatos.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int entero=10;
+    float flotante=8.3;
+    double doble=6.6563;
+    char caracter='c';
+    std::cout<<caracter;
+    std::cin.get();
+    return 0;
+}
