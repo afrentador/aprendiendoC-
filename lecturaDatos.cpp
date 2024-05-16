0d69b1fb95208e4e76c74a802b2bb27c5949fcfe
Victor Mendoza <vmendozaespina@gmail.com>
2024-05-16 12:15:45 -0500

    trabajando con operaciones matematicas

---
+++ lecturaDatos.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    float numero;
+    std::cout<<"humano !! ingresa un numero entero: ";
+    std::cin>>numero;
+    std::cout<<"humano este es el numero que ingresaste: ";
+    std::cout<<numero;
+    std::cout<<"\n";
+    //std::cin.get();
+    system("pause");
+    return 0;
+}
+
+/*manera de poder hacer 
+comentarios para grupo de
+lineas*/
